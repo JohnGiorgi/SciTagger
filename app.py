ac6379a90161b10c2093e426f5a2575e845e0de2
John Giorgi <johnmgiorgi@gmail.com>
2023-04-22 21:44:39 -0400

    feat: add support for s2 api

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from typing import List
 
+import requests
 import streamlit as st
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
@@ -130,11 +131,22 @@ def main():
         """
     )
 
+    with st.expander("Observations 🧐"):
+        st.write(
+            """
+            - OpenAI models currently perform the best. I was not able to solve the task at all with Cohere models.
+            - `gpt.3.5-turbo` performs reasonably well, I did not try `gpt-4` but assume it would perform better.
+            - The model appears to perform better on longer texts. Performance on short texts is not great.
+            - The model appears to perform the best for biomedical texts. It also performs well for the natural sciences (e.g. physics), but slightly worse for computer science.
+            - I am simply using NLTK for sentence tokenization, which is likely not optimal. Some sentences clearly belong to multiple tags, which would require sub sentence tokenization.
+            """
+        )
+
     # Second section
-    st.subheader("How to use this demo ❓")
+    st.subheader("How to use this demo")
     st.write(
         """
-        Enter a sentence or paragraph of text from a scientific paper (or choose one of the examples). Each sentence will be classified as one of the following types:
+        Enter a sentence or paragraph of text from a scientific paper, a papers [Semantic Scholar](https://www.semanticscholar.org) ID, or choose one of the provided examples. Each sentence will be classified as one of the following types:
         
 - __Goal__: Research goal
 - __Fact__: A known fact, a statement taken to be true by the author
@@ -151,51 +163,74 @@ def main():
         "Discourse tags come from ([de Waard & Pander Maat, 2012](https://aclanthology.org/W12-4306/))."
     )
 
+    text_tab, s2_tab, examples_tab = st.tabs(["Text", "Semantic Scholar", "Examples"])
+
     # Third section
     if api_key:
-        input_text = st.text_area(
-            "Enter some text here",
-            disabled=False,
-            placeholder="Sentence or paragraph of text from a scientific paper...",
-            help="test",
-        )
+        with text_tab:
+            input_text = st.text_area(
+                "Enter some text",
+                placeholder="Sentence or paragraph of text from a scientific paper...",
+            )
 
-        st.write("Or, choose one of the examples below:")
+        with s2_tab:
+            s2_id = st.text_input(
+                "Enter a Semantic Scholar ID",
+                placeholder="Enter a Semantic Scholar ID, e.g. 204e3073870fae3d05bcbc2f6a8e263d9b72e776",
+            )
+            if s2_id:
+                r = requests.post(
+                    "https://api.semanticscholar.org/graph/v1/paper/batch",
+                    params={"fields": "abstract"},
+                    json={"ids": [s2_id]},
+                )
+                if r.status_code == 200:
+                    input_text = r.json()[0].get("abstract")
+                    if input_text is None:
+                        st.warning(
+                            "No abstract found for this paper. Please try another ID."
+                        )
+                else:
+                    st.warning(
+                        f"There was an error with the Semantic Scholar API (status code {r.status_code}). Please try again."
+                    )
+            st.caption("The abstract of the paper will be retrieved and used as input.")
 
-        # Three horizontal buttons
-        col1, col2, col3, col4 = st.columns(4)
-        with col1:
-            example_biomedical = st.button("Example 1 (Biomedical)")
-        with col2:
-            example_compsci = st.button("Example 2 (CompSci)")
-        with col3:
-            example_physics = st.button("Example 3 (Physics)")
-        with col4:
-            example_chemistry = st.button("Example 4 (Chemistry)")
-        st.caption(
-            "At the time of writing, all examples are from scientific papers published _after_ GPT-3/3.5/4's data cutoff date of September 2021."
-        )
+        with examples_tab:
+            # Three horizontal buttons
+            col1, col2, col3, col4 = st.columns(4)
+            with col1:
+                example_biomedical = st.button("Example 1 (Biomedical)")
+            with col2:
+                example_compsci = st.button("Example 2 (CompSci)")
+            with col3:
+                example_physics = st.button("Example 3 (Physics)")
+            with col4:
+                example_chemistry = st.button("Example 4 (Chemistry)")
+            st.caption(
+                "At the time of writing, all examples are from scientific papers published _after_ GPT-3/3.5/4's data cutoff date of September 2021."
+            )
 
-        if example_biomedical:
-            input_text = "For unknown reasons, the melanocyte stem cell (McSC) system fails earlier than other adult stem cell populations1, which leads to hair greying in most humans and mice2,3. Current dogma states that McSCs are reserved in an undifferentiated state in the hair follicle niche, physically segregated from differentiated progeny that migrate away following cues of regenerative stimuli4-8. Here we show that most McSCs toggle between transit-amplifying and stem cell states for both self-renewal and generation of mature progeny, a mechanism fundamentally distinct from those of other self-renewing systems. Live imaging and single-cell RNA sequencing revealed that McSCs are mobile, translocating between hair follicle stem cell and transit-amplifying compartments where they reversibly enter distinct differentiation states governed by local microenvironmental cues (for example, WNT). Long-term lineage tracing demonstrated that the McSC system is maintained by reverted McSCs rather than by reserved stem cells inherently exempt from reversible changes. During ageing, there is accumulation of stranded McSCs that do not contribute to the regeneration of melanocyte progeny. These results identify a new model whereby dedifferentiation is integral to homeostatic stem cell maintenance and suggest that modulating McSC mobility may represent a new approach for the prevention of hair greying."
-            st.write(
-                "##### Example 1: [Dedifferentiation maintains melanocyte stem cells in a dynamic niche](https://pubmed.ncbi.nlm.nih.gov/37076619/)"
-            )
-        elif example_compsci:
-            input_text = "We report the development of GPT-4, a large-scale, multimodal model which can accept image and text inputs and produce text outputs. While less capable than humans in many real-world scenarios, GPT-4 exhibits human-level performance on various professional and academic benchmarks, including passing a simulated bar exam with a score around the top 10% of test takers. GPT-4 is a Transformer-based model pre-trained to predict the next token in a document. The post-training alignment process results in improved performance on measures of factuality and adherence to desired behavior. A core component of this project was developing infrastructure and optimization methods that behave predictably across a wide range of scales. This allowed us to accurately predict some aspects of GPT-4's performance based on models trained with no more than 1/1,000th the compute of GPT-4."
-            st.write(
-                "##### Example 2: [GPT-4 Technical Report](https://arxiv.org/abs/2303.08774)"
-            )
-        elif example_physics:
-            input_text = "Our Moon periodically moves through the magnetic tail of the Earth that contains terrestrial ions of hydrogen and oxygen. A possible density contrast might have been discovered that could be consistent with the presence of water phase of potential terrestrial origin. Using novel gravity aspects (descriptors) derived from harmonic potential coefficients of gravity field of the Moon, we discovered gravity strike angle anomalies that point to water phase locations in the polar regions of the Moon. Our analysis suggests that impact cratering processes were responsible for specific pore space network that were subsequently filled with the water phase filling volumes of permafrost in the lunar subsurface. In this work, we suggest the accumulation of up to ~ 3000 km3 of terrestrial water phase (Earth’s atmospheric escape) now filling the pore spaced regolith, portion of which is distributed along impact zones of the polar regions of the Moon. These unique locations serve as potential resource utilization sites for future landing exploration and habitats (e.g., NASA Artemis Plan objectives)."
-            st.write(
-                "##### Example 3: [Distribution of water phase near the poles of the Moon from gravity aspects](https://www.nature.com/articles/s41598-022-08305-x)"
-            )
-        elif example_chemistry:
-            input_text = "Given the increasing consumer demand for raw, nonprocessed, safe, and long shelf-life fish and seafood products, research concerning the application of natural antimicrobials as alternatives to preservatives is of great interest. The aim of the following paper was to evaluate the effect of essential oils (EOs) from black pepper (BPEO) and tarragon (TEO), and their bioactive compounds: limonene (LIM), β-caryophyllene (CAR), methyl eugenol (ME), and β-phellandrene (PHE) on the lipolytic activity and type II secretion system (T2SS) of Pseudomonas psychrophila KM02 (KM02) fish isolates grown in vitro and in fish model conditions. Spectrophotometric analysis with the p-NPP reagent showed inhibition of lipolysis from 11 to 46%. These results were confirmed by RT-qPCR, as the expression levels of lipA, lipB, and genes encoding T2SS were also considerably decreased. The supplementation of marinade with BPEO and TEO contributed to KM02 growth inhibition during vacuum packaging of salmon fillets relative to control samples. Whole-genome sequencing (WGS) provided insight into the spoilage potential of KM02, proving its importance as a spoilage microorganism whose metabolic activity should be inhibited to maintain the quality and safety of fresh fish in the food market."
-            st.write(
-                "##### Example 4: [Black pepper and tarragon essential oils suppress the lipolytic potential and the type II secretion system of P. psychrophila KM02](https://www.nature.com/articles/s41598-022-09311-9)"
-            )
+            if example_biomedical:
+                input_text = "For unknown reasons, the melanocyte stem cell (McSC) system fails earlier than other adult stem cell populations1, which leads to hair greying in most humans and mice2,3. Current dogma states that McSCs are reserved in an undifferentiated state in the hair follicle niche, physically segregated from differentiated progeny that migrate away following cues of regenerative stimuli4-8. Here we show that most McSCs toggle between transit-amplifying and stem cell states for both self-renewal and generation of mature progeny, a mechanism fundamentally distinct from those of other self-renewing systems. Live imaging and single-cell RNA sequencing revealed that McSCs are mobile, translocating between hair follicle stem cell and transit-amplifying compartments where they reversibly enter distinct differentiation states governed by local microenvironmental cues (for example, WNT). Long-term lineage tracing demonstrated that the McSC system is maintained by reverted McSCs rather than by reserved stem cells inherently exempt from reversible changes. During ageing, there is accumulation of stranded McSCs that do not contribute to the regeneration of melanocyte progeny. These results identify a new model whereby dedifferentiation is integral to homeostatic stem cell maintenance and suggest that modulating McSC mobility may represent a new approach for the prevention of hair greying."
+                st.write(
+                    "##### Example 1: [Dedifferentiation maintains melanocyte stem cells in a dynamic niche](https://pubmed.ncbi.nlm.nih.gov/37076619/)"
+                )
+            elif example_compsci:
+                input_text = "We report the development of GPT-4, a large-scale, multimodal model which can accept image and text inputs and produce text outputs. While less capable than humans in many real-world scenarios, GPT-4 exhibits human-level performance on various professional and academic benchmarks, including passing a simulated bar exam with a score around the top 10% of test takers. GPT-4 is a Transformer-based model pre-trained to predict the next token in a document. The post-training alignment process results in improved performance on measures of factuality and adherence to desired behavior. A core component of this project was developing infrastructure and optimization methods that behave predictably across a wide range of scales. This allowed us to accurately predict some aspects of GPT-4's performance based on models trained with no more than 1/1,000th the compute of GPT-4."
+                st.write(
+                    "##### Example 2: [GPT-4 Technical Report](https://arxiv.org/abs/2303.08774)"
+                )
+            elif example_physics:
+                input_text = "Our Moon periodically moves through the magnetic tail of the Earth that contains terrestrial ions of hydrogen and oxygen. A possible density contrast might have been discovered that could be consistent with the presence of water phase of potential terrestrial origin. Using novel gravity aspects (descriptors) derived from harmonic potential coefficients of gravity field of the Moon, we discovered gravity strike angle anomalies that point to water phase locations in the polar regions of the Moon. Our analysis suggests that impact cratering processes were responsible for specific pore space network that were subsequently filled with the water phase filling volumes of permafrost in the lunar subsurface. In this work, we suggest the accumulation of up to ~ 3000 km3 of terrestrial water phase (Earth’s atmospheric escape) now filling the pore spaced regolith, portion of which is distributed along impact zones of the polar regions of the Moon. These unique locations serve as potential resource utilization sites for future landing exploration and habitats (e.g., NASA Artemis Plan objectives)."
+                st.write(
+                    "##### Example 3: [Distribution of water phase near the poles of the Moon from gravity aspects](https://www.nature.com/articles/s41598-022-08305-x)"
+                )
+            elif example_chemistry:
+                input_text = "Given the increasing consumer demand for raw, nonprocessed, safe, and long shelf-life fish and seafood products, research concerning the application of natural antimicrobials as alternatives to preservatives is of great interest. The aim of the following paper was to evaluate the effect of essential oils (EOs) from black pepper (BPEO) and tarragon (TEO), and their bioactive compounds: limonene (LIM), β-caryophyllene (CAR), methyl eugenol (ME), and β-phellandrene (PHE) on the lipolytic activity and type II secretion system (T2SS) of Pseudomonas psychrophila KM02 (KM02) fish isolates grown in vitro and in fish model conditions. Spectrophotometric analysis with the p-NPP reagent showed inhibition of lipolysis from 11 to 46%. These results were confirmed by RT-qPCR, as the expression levels of lipA, lipB, and genes encoding T2SS were also considerably decreased. The supplementation of marinade with BPEO and TEO contributed to KM02 growth inhibition during vacuum packaging of salmon fillets relative to control samples. Whole-genome sequencing (WGS) provided insight into the spoilage potential of KM02, proving its importance as a spoilage microorganism whose metabolic activity should be inhibited to maintain the quality and safety of fresh fish in the food market."
+                st.write(
+                    "##### Example 4: [Black pepper and tarragon essential oils suppress the lipolytic potential and the type II secretion system of P. psychrophila KM02](https://www.nature.com/articles/s41598-022-09311-9)"
+                )
 
         if input_text.strip():
             sentences = sent_tokenize(input_text)
@@ -223,26 +258,16 @@ def main():
             st.write("##### Model output 🤖")
             st.markdown(formatted_text, unsafe_allow_html=True)
     else:
-        input_text = st.text_area("Enter your text here", value="", disabled=True)
-        st.warning(
-            "Please enter your API key in the sidebar to enable the input text box and examples."
-        )
+        with text_tab:
+            st.warning(
+                "Please enter your API key in the sidebar to enable the input text box and examples."
+            )
+        with examples_tab:
+            st.warning(
+                "Please enter your API key in the sidebar to enable the input text box and examples."
+            )
 
     # Fourth section
-    "---"
-    st.subheader("Observations 🧐")
-    st.write(
-        """
-        - OpenAI models currently perform the best. I was not able to solve the task at all with Cohere models.
-        - `gpt.3.5-turbo` performs reasonably well, I did not try `gpt-4` but assume it would perform better.
-        - The model appears to perform better on longer texts. Performance on short texts is not great.
-        - The model appears to perform the best for biomedical texts. It also performs well for the natural sciences (e.g. physics), but slightly worse for computer science.
-        - All examples are scientific papers published _after_ GPT-3/3.5/4's data cutoff date of September 2021.
-        - I am simply using NLTK for sentence tokenization, which is likely not optimal. Some sentences clearly belong to multiple tags, which would require sub sentence tokenization.
-        """
-    )
-
-    # Fith section
     st.subheader("Feedback 📝")
     st.write(
         "Feel free to leave an issue or open a pull request on [GitHub](https://github.com/JohnGiorgi/SciTagger)!"
